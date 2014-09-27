e9326074622418717c215ba88b28f1f38b6c3405
Wan Kim Mok <cafe.mui@gmail.com>
2014-09-26 23:49:40 -0400

    slight modify for scan function uses the second arg value as file input

--- scanner.py
+++ scanner.py
@@ -1,18 +1,17 @@
 import sys
 
 def scan(input):
-	for a in input:
+	output = open(input).read()
+	
+	for a in output:
 		print a
 
 
 if __name__ == '__main__':
-	scan("Hello World")
 	if len(sys.argv)!= 2:
 		exit(1)
 
 	#Open file
 	filename = sys.argv[1]
 	#Read file
-	output = open(filename).read()
-	#print file
-	print output
+	scan(filename)
