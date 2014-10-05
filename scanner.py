4eb7a05c85b85ae63c757980f060280eed8082db
Wan Kim Mok <cafe.mui@gmail.com>
2014-10-04 22:32:23 -0400

    Modify Scanner class to include the scan, and build_string function

--- scanner.py
+++ scanner.py
@@ -1,68 +1,58 @@
 import sys
 
 class Scanner(object):
-	def __init__(self, cur_row, cur_col, string_mode):
-		self.cur_row = cur_row
-		self.cur_col = cur_col
-		self.string_mode = string_mode
+    def __init__(self, cur_row, cur_col, string_mode, string_rec, count):
+        #keep track of current row
+        self.cur_row = cur_row
+        #keep track of current column
+        self.cur_col = cur_col
+        #check if in string mode
+        self.string_mode = string_mode
+        self.string_rec = string_rec
+        self.count = count
 
-	def scan(self, input):
-		output = open(input, 'r').read().splitlines()
-		for line in output:
-			self.cur_row +=1
-			self.cur_col +=1
-			for a in line:
-				self.cur_col +=1
+    def scan(self, input):
+        output = open(input, 'r').read().splitlines()
+        for line in output:
+            self.cur_row +=1
+            self.cur_col =1
+            for a in line:
+            #if a double quotation is seen
+                if ord(a) == 34:
+                    self.count +=1
+                    self.string_mode = True
+
+                if self.string_mode == True:
+                    self.build_string(a)
+
+                self.cur_col +=1
+
+    def build_string(self, a):
+        if self.count == 2:
+            self.string_rec += a
+            print "Row " + str(self.cur_row-1) +" : "+ str(self.string_rec) + " is a string."
+            self.string_rec = ''
+            self.count = 0
+            self.string_mode = False
+            return
+        else:
+            self.string_rec += a
+            return
+
 
 	#learning purpose
 	def show(self):
 		print self.cur_row
 
+    
+    keyword={
+        'STRING'  : 'TK_STRING',
+        'PROGRAM' : 'TK_PROGRAM'
+    }        
 
 
 
-#######################################################
 
-def scan(input):
-	string_c= 0
-	string_rec = ''
-	string_mode = False
-	identifier_mode = False
-
-	#TOKEN
-	keyword={
-		'STRING'  : 'TK_STRING',
-		'PROGRAM' : 'TK_PROGRAM'
-	}
-
-	#keep track of current row
-	cur_row = 1
-	#keep track of current column
-	cur_col = 1
-
-	output = open(input, 'r').read().splitlines()
-	for line in output:
-		#print "current row scanned:" +str(cur_row)+ ":" +line
-		cur_row+=1
-		cur_col =1
-		for a in line:
-			#print "currrent column scanned:"+str(cur_col)+":" +a
-			if a == '"':
-				string_c +=1
-				string_mode = True
-
-			# if in string mode append characters
-			if string_mode:
-				string_rec += str(a)
-
-				if string_c == 2 :
-					print "Row " + str(cur_row) +": "+ string_rec + " is a string."
-					string_c=0
-					string_rec=''
-
-				if string_c == 0:
-					string_mode = False
-			cur_col+=1
 
 
 if __name__ == '__main__':
@@ -71,8 +61,6 @@ if __name__ == '__main__':
 
 	#Open file
 	filename = sys.argv[1]
-	#Read file
-	scan(filename)
 
-	a = Scanner(1,1,False)
-	a.show()
+	a = Scanner(1,1,False,'', 0)
+	a.scan(filename)
