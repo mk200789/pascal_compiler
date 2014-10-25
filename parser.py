3c2a3ec22faf8b37dd684235357f1e2122411d45
Wan Kim Mok <cafe.mui@gmail.com>
2014-10-24 21:53:20 -0400

    created a parser class

--- parser.py
+++ parser.py
@@ -1,16 +1,26 @@
-def get_token(alist):
-	#get token information
-	#for x in alist:
-	#	print x
-	t = iter(alist)
-	size = len(alist)
-	while (size):
-		print next(t)
-		size -=1
+class Parser(object):
+	def __init__(self, alist):
+		self.alist = alist
+
+	def parse(self):
+		self.retrieve()
+
+	def get_token(self, alist):
+		t = iter(self.alist)
+		size = len(self.alist)
+		while (size):
+			print next(t)
+			print size
+			size -=1
+
+	def retrieve(self):
+		self.get_token(self.alist)
 
 if __name__ == '__main__':
 
 	#Open file
 	alist = [('TK_PROGRAM', 'program', 1, 7), ('TK_IDENTIFIER', 'AddExample', 1, 18), ('TK_SEMICOLON', ';', 1, 19)]
 
-	get_token(alist)
+	#get_token(alist)
+	a = Parser(alist)
+	a.parse()
