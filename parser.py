fb88c8501288c9d9e7228ea4742a92005c6a4382
Wan Kim Mok <cafe.mui@gmail.com>
2014-10-27 19:28:56 -0400

    added a setup_iterator function to iter tokens

--- parser.py
+++ parser.py
@@ -1,19 +1,23 @@
 class Parser(object):
-	def __init__(self, alist, ):
+	def __init__(self, alist, cur_token):
 		self.alist = alist
+		self.cur_token = cur_token
+		self.iterator = self.setup_iterator()
 
 	def parse(self):
-		self.retrieve()
+		self.get_token()
 
-	def get_token(self, alist):
-		t = iter(self.alist)
-		size = len(self.alist)
-		while (size):
-			print next(t)
-			size -=1
 
-	def retrieve(self):
-		self.get_token(self.alist)
+	def setup_iterator(self):
+		tokens = iter(self.alist)
+		return tokens
+
+	def get_token(self):
+		self.cur_token = self.iterator.next()
+		return
+
+	#def retrieve(self):
+	#	self.get_token()
 
 
 if __name__ == '__main__':
@@ -22,5 +26,5 @@ if __name__ == '__main__':
 	alist = [('TK_PROGRAM', 'program', 1, 7), ('TK_IDENTIFIER', 'AddExample', 1, 18), ('TK_SEMICOLON', ';', 1, 19)]
 
 	#get_token(alist)
-	a = Parser(alist)
+	a = Parser(alist, 0)
 	a.parse()
