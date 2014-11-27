6ca558df3b919ac60e60c210e12cd8701d058b8e
Wan Kim Mok <cafe.mui@gmail.com>
2014-11-26 22:59:28 -0500

    created function simulate()

--- simulator.py
+++ simulator.py
@@ -3,3 +3,12 @@ class Simulator(object):
 	def __init__(self, symtable, d_nodes):
 		self.symtable = symtable
 		self.d_nodes = d_nodes
+
+
+	def simulate(self, d_nodes):
+		for d in d_nodes:
+			print d
+
+
+
+#if __name__ == '__main__':
