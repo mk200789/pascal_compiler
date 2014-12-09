c665603fb8404186781bac0e8da1da67be9e883a
Wan Kim Mok <cafe.mui@gmail.com>
2014-12-09 15:29:34 -0500

    this file will run parser, scanner, and simulator

---
+++ compiler/run.py
@@ -0,0 +1,23 @@
+from scanner import Scanner
+from parser import Parser
+from simulator import Simulator
+import sys
+
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		print "invalid argument"
+		sys.exit(0)
+
+	filename = sys.argv[1]
+
+	#set up scanner
+	scanner = Scanner(1,1,False,'', False, False,[],'',[], False)
+	tokens = scanner.scan(filename)
+	
+	#set parser
+	parser = Parser(tokens, 0)
+	e = parser.parse()
+
+	#set simulator
+	simulator = Simulator(e['symtable'], e['d_nodes'])
+	simulate = simulator.simulate()
