aa41c75b8c6b88fb7decbf5209551560d6c58785
Wan Kim Mok <cafe.mui@gmail.com>
2014-12-28 22:46:49 -0500

    remove prints

--- compiler/run.py
+++ compiler/run.py
@@ -6,6 +6,7 @@ import sys
 if __name__ == '__main__':
 	if len(sys.argv) != 2:
 		print "invalid argument"
+		print "python /compiler/run.py examples/test code"
 		sys.exit(0)
 
 	filename = sys.argv[1]
